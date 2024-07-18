4584601703bd8c29521b64771f13dce578548e37
vikalp026var <vikalp026varshney@gmail.com>
2024-07-18 15:47:37 +0530

    update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='ViviAI',
+    description="A project for starlab Capital",
+    author="Vikalp",
+    author_email="vikalp026varshney@gmail.com",
+    packages=find_packages(),
+    include_dirs=[]
+)
